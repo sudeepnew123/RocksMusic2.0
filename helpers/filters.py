b70b696cbffda27839761a02dbe74392ded07d5c
sudeepnew123 <sudeepmandal8999@gmail.com>
2025-05-10 18:14:48 +0530

    Update filters.py

--- helpers/filters.py
+++ helpers/filters.py
@@ -16,12 +16,17 @@
 
 
 from typing import List, Union
+
 from pyrogram import filters
+
 from config import COMMAND_PREFIXES
 
-# Updated filters without 'edited'
-other_filters = filters.group & ~filters.via_bot & ~filters.forwarded
-other_filters2 = filters.private & ~filters.via_bot & ~filters.forwarded
+other_filters = filters.group & ~filters.edited & ~filters.via_bot & ~filters.forwarded
+other_filters2 = (
+    filters.private & ~filters.edited & ~filters.via_bot & ~filters.forwarded
+)
+
 
 def command(commands: Union[str, List[str]]):
     return filters.command(commands, COMMAND_PREFIXES)
+    
